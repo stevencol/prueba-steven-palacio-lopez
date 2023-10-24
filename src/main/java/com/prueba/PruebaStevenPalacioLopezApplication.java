3079286b03ba1470bb01bef1b26d5426d270e6c4
stevencol <steven2088@hotmail.es>
2023-10-23 21:11:13 -0500

    Se inicializa repositorio, utilizando java 17 , springboot con la dependencias de Lombok, validate, sping data jpa, mysql

---
+++ src/main/java/com/prueba/PruebaStevenPalacioLopezApplication.java
@@ -0,0 +1,13 @@
+package com.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaStevenPalacioLopezApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaStevenPalacioLopezApplication.class, args);
+    }
+
+}
